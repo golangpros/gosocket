f89e8e92c7b4bad0c042cae722b2a033be072088
krishpranav <krisna.pranav@gmail.com>
2022-01-16 14:15:48 +0530

    connection

--- conn.go
+++ conn.go
@@ -1,10 +1,18 @@
 package gosocket
 
-import "net"
+import (
+	"net"
+	"time"
+)
 
 type Conn struct {
-	sid     string
-	rawConn net.Conn
-	sendCh  chan []byte
-	done    chan error
+	sid        string
+	rawConn    net.Conn
+	sendCh     chan []byte
+	done       chan error
+	hbTimer    *time.Timer
+	name       string
+	messageCh  chan *Message
+	hbInterval time.Duration
+	hbTimeout  time.Duration
 }
