74392aad9074df8b4c9556886963e14829633b20
krishpranav <krisna.pranav@gmail.com>
2022-01-16 17:19:35 +0530

    connection

--- conn.go
+++ conn.go
@@ -16,3 +16,18 @@ type Conn struct {
 	hbInterval time.Duration
 	hbTimeout  time.Duration
 }
+
+func (c *Conn) getName() string {
+	return c.name
+}
+
+func NewConn(c net.Conn, hbInterval time.Duration, hbTimeout time.Duration) *Conn {
+	conn := &Conn{}
+	conn.name = c.RemoteAddr().String()
+	conn.hbTimer = time.NewTimer(conn.hbInterval)
+
+	if conn.hbInterval == 0 {
+		conn.hbTimer.Stop()
+	}
+	return conn
+}
