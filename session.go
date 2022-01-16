59714bc880f386643d33bcb65acdc9840a279f8c
krishpranav <krisna.pranav@gmail.com>
2022-01-16 14:08:33 +0530

    session

--- session.go
+++ session.go
@@ -1,8 +1,22 @@
 package gosocket
 
+import uuid "github.com/satori/go.uuid"
+
 type Session struct {
 	ID       string
 	UID      string
 	conn     *Conn
 	settings map[string]interface{}
+}
+
+func newSession(conn *Conn) *Session {
+	id, _ := uuid.NewV4()
+	session := &Session{
+		ID:       id.String(),
+		UID:      "",
+		conn:     conn,
+		settings: make(map[string]interface{}),
+	}
+
+	return session
 }
