6bac86f491ffe240892069dc31a6578b5eae8597
Williams <82570807+NukeWilliams@users.noreply.github.com>
2022-01-16 14:12:26 +0530

    session

--- session.go
+++ session.go
@@ -1,9 +1,11 @@
 package gosocket
 
-import uuid "github.com/satori/go.uuid"
+import (
+	uuid "github.com/satori/go.uuid"
+)
 
 type Session struct {
-	ID       string
+	ID      string
 	UID      string
 	conn     *Conn
 	settings map[string]interface{}
@@ -12,7 +14,7 @@ type Session struct {
 func newSession(conn *Conn) *Session {
 	id, _ := uuid.NewV4()
 	session := &Session{
-		ID:       id.String(),
+		ID:      id.String(),
 		UID:      "",
 		conn:     conn,
 		settings: make(map[string]interface{}),
@@ -25,11 +27,11 @@ func (s *Session) getSessionID() string {
 	return s.ID
 }
 
-func (s *Session) bindUserId(uid string) {
+func (s *Session) bindUserID(uid string) {
 	s.UID = uid
 }
 
-func (s *Session) getUserId() string {
+func (s *Session) getUserID() string {
 	return s.UID
 }
 
@@ -39,4 +41,17 @@ func (s *Session) getConn() *Conn {
 
 func (s *Session) setConn(conn *Conn) {
 	s.conn = conn
+}
+
+func (s *Session) getSetting(key string) interface{} {
+
+	if v, ok := s.settings[key]; ok {
+		return v
+	}
+
+	return nil
+}
+
+func (s *Session) setSetting(key string, value interface{}) {
+	s.settings[key] = value
 }
