288e30760aed8ad300e827c84e73ca7c19422acd
krishpranav <krisna.pranav@gmail.com>
2022-01-16 17:22:25 +0530

    conn

--- conn.go
+++ conn.go
@@ -1,6 +1,10 @@
 package gosocket
 
 import (
+	"bytes"
+	"context"
+	"encoding/binary"
+	"io"
 	"net"
 	"time"
 )
@@ -17,27 +21,36 @@ type Conn struct {
 	hbTimeout  time.Duration
 }
 
-func (c *Conn) getName() string {
+func (c *Conn) GetName() string {
 	return c.name
 }
 
 func NewConn(c net.Conn, hbInterval time.Duration, hbTimeout time.Duration) *Conn {
-	conn := &Conn{}
+	conn := &Conn{
+		rawConn:    c,
+		sendCh:     make(chan []byte, 100),
+		done:       make(chan error),
+		messageCh:  make(chan *Message, 100),
+		hbInterval: hbInterval,
+		hbTimeout:  hbTimeout,
+	}
+
 	conn.name = c.RemoteAddr().String()
 	conn.hbTimer = time.NewTimer(conn.hbInterval)
 
 	if conn.hbInterval == 0 {
 		conn.hbTimer.Stop()
 	}
+
 	return conn
 }
 
-func (c *Conn) close() {
+func (c *Conn) Close() {
 	c.hbTimer.Stop()
 	c.rawConn.Close()
 }
 
-func (c *Conn) sendMessage(msg *Message) error {
+func (c *Conn) SendMessage(msg *Message) error {
 	pkg, err := Encode(msg)
 	if err != nil {
 		return err
@@ -45,4 +58,86 @@ func (c *Conn) sendMessage(msg *Message) error {
 
 	c.sendCh <- pkg
 	return nil
+}
+
+func (c *Conn) writeCoroutine(ctx context.Context) {
+	hbData := make([]byte, 0)
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		case pkt := <-c.sendCh:
+
+			if pkt == nil {
+				continue
+			}
+
+			if _, err := c.rawConn.Write(pkt); err != nil {
+				c.done <- err
+			}
+
+		case <-c.hbTimer.C:
+			hbMessage := NewMessage(msgHeartbeat, hbData)
+			c.SendMessage(hbMessage)
+			if c.hbInterval > 0 {
+				c.hbTimer.Reset(c.hbInterval)
+			}
+		}
+	}
+}
+
+func (c *Conn) readCoroutine(ctx context.Context) {
+
+	for {
+		select {
+		case <-ctx.Done():
+			return
+
+		default:
+			if c.hbInterval > 0 {
+				err := c.rawConn.SetReadDeadline(time.Now().Add(c.hbTimeout))
+				if err != nil {
+					c.done <- err
+					continue
+				}
+			}
+
+			buf := make([]byte, 4)
+			_, err := io.ReadFull(c.rawConn, buf)
+			if err != nil {
+				c.done <- err
+				continue
+			}
+
+			bufReader := bytes.NewReader(buf)
+
+			var dataSize int32
+			err = binary.Read(bufReader, binary.LittleEndian, &dataSize)
+			if err != nil {
+				c.done <- err
+				continue
+			}
+
+			databuf := make([]byte, dataSize)
+			_, err = io.ReadFull(c.rawConn, databuf)
+			if err != nil {
+				c.done <- err
+				continue
+			}
+
+			msg, err := Decode(databuf)
+			if err != nil {
+				c.done <- err
+				continue
+			}
+
+			if msg.GetID() == msgHeartbeat {
+				continue
+			}
+
+			c.messageCh <- msg
+		}
+	}
 }
