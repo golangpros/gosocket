af1bda51f30f0f48cce90d9cf239b280252bba87
krishpranav <krisna.pranav@gmail.com>
2022-01-16 17:30:00 +0530

    codec

--- codec.go
+++ codec.go
@@ -3,6 +3,7 @@ package gosocket
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 )
 
 func Encode(msg *Message) ([]byte, error) {
@@ -26,4 +27,40 @@ func Encode(msg *Message) ([]byte, error) {
 	}
 
 	return buffer.Bytes(), nil
+}
+
+func Decode(data []byte) (*Message, error) {
+	bufReader := bytes.NewReader(data)
+
+	dataSize := len(data)
+	var msgID int32
+	err := binary.Read(bufReader, binary.LittleEndian, &msgID)
+	if err != nil {
+		return nil, err
+	}
+
+	dataBufLength := dataSize - 4 - 4
+	dataBuf := make([]byte, dataBufLength)
+	err = binary.Read(bufReader, binary.LittleEndian, &dataBuf)
+	if err != nil {
+		return nil, err
+	}
+
+	var checksum uint32
+	err = binary.Read(bufReader, binary.LittleEndian, &checksum)
+	if err != nil {
+		return nil, err
+	}
+
+	message := &Message{}
+	message.msgSize = int32(dataSize)
+	message.msgID = msgID
+	message.data = dataBuf
+	message.checksum = checksum
+
+	if message.Verify() {
+		return message, nil
+	}
+
+	return nil, errors.New("checksum error")
 }
